2910cbf0be875fbc016ab2457b8bb763d3994952
minog9959-png <minog9959@gmail.com>
2026-07-18 04:02:22 +0500

    Fix Railway deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://fashio-x565.vercel.app/",
+    origin: "https://fashio-x565.vercel.app",
     // origin:https://fashio-x565.vercel.app/
     credentials: true,
   })
