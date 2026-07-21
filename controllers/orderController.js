c73163228574d2384e04b1213de30e161cd8ab77
minog9959-png <minog9959@gmail.com>
2026-07-21 15:55:29 +0500

    backendcompleted

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -25,3 +25,61 @@ export const createOrder = async (req, res) => {
     });
   }
 };
+
+// get Order
+export const getUserOrders = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const orders = await Order.find({
+      user: userId,
+    })
+      .populate("user")
+      .populate("items.product");
+
+    res.status(200).json({
+      success: true,
+      count: orders.length,
+      orders,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//update order:
+export const updateOrderStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found",
+      });
+    }
+
+    order.status = status;
+
+    await order.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Order status updated",
+      order,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { createOrder } from "../controllers/orderController.js";
+import { createOrder, getUserOrders, updateOrderStatus } from "../controllers/orderController.js";
 
 const router  = express.Router();
 router.post("/",createOrder);
+router.get("/:userId",getUserOrders);
+router.put("/:id",updateOrderStatus);
 export default router;
