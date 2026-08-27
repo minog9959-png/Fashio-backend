885bff7e08a1fd985838a0ddc886703e675bb599
minog9959-png <minog9959@gmail.com>
2026-08-27 15:18:49 +0500

    Fix Firebase Admin configuration

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,13 +1,31 @@
+// import { initializeApp, cert } from "firebase-admin/app";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const serviceAccount = {
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//   privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+// };
+
+// const firebaseAdmin = initializeApp({
+//   credential: cert(serviceAccount),
+// });
+
+// export default firebaseAdmin;
+
 import { initializeApp, cert } from "firebase-admin/app";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-};
+const serviceAccount = JSON.parse(
+  Buffer.from(
+    process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
+    "base64"
+  ).toString("utf8")
+);
 
 const firebaseAdmin = initializeApp({
   credential: cert(serviceAccount),
